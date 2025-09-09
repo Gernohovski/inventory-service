c333ed4e2f09a8f4a48733750ec96f83a857a482
Rodrigo Gernohovski <mra_rgernohovski@veloecorp.com.br>
2025-09-08 21:14:53 -0300

    chore: Aplica correção de identação e otimização de imports

--- src/main/java/br/com/fatec/mogi/inventory_service/coreService/domain/model/CategoriaItem.java
+++ src/main/java/br/com/fatec/mogi/inventory_service/coreService/domain/model/CategoriaItem.java
@@ -1,7 +1,11 @@
 package br.com.fatec.mogi.inventory_service.coreService.domain.model;
 
 import jakarta.persistence.Entity;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @Getter
--- src/main/java/br/com/fatec/mogi/inventory_service/coreService/web/CategoriaItemController.java
+++ src/main/java/br/com/fatec/mogi/inventory_service/coreService/web/CategoriaItemController.java
@@ -5,7 +5,11 @@ import br.com.fatec.mogi.inventory_service.coreService.web.request.CadastrarCate
 import br.com.fatec.mogi.inventory_service.coreService.web.response.BuscarCategoriasResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/core-service/v1/categorias")
