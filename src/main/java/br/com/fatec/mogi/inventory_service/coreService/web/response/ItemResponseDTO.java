ce62ce0302c5b39fba70c52706dcbb0cdc8dabd6
Rodrigo Gernohovski <mra_rgernohovski@veloecorp.com.br>
2025-09-10 22:39:36 -0300

    feat: Adiciona endpoint de consulta de itens com base em um filtro informado

---
+++ src/main/java/br/com/fatec/mogi/inventory_service/coreService/web/response/ItemResponseDTO.java
@@ -0,0 +1,40 @@
+package br.com.fatec.mogi.inventory_service.coreService.web.response;
+
+import br.com.fatec.mogi.inventory_service.coreService.domain.model.CategoriaItem;
+import br.com.fatec.mogi.inventory_service.coreService.domain.model.Localizacao;
+import br.com.fatec.mogi.inventory_service.coreService.domain.model.StatusItem;
+import br.com.fatec.mogi.inventory_service.coreService.domain.model.TipoEntrada;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemResponseDTO {
+
+	private String nomeItem;
+
+	private String numeroSerie;
+
+	private StatusItem statusItem;
+
+	private CategoriaItem categoriaItem;
+
+	private TipoEntrada tipoEntrada;
+
+	private String codigoItem;
+
+	private LocalDateTime dataCadastro;
+
+	private Localizacao localizacao;
+
+	private String notaFiscal;
+
+}
