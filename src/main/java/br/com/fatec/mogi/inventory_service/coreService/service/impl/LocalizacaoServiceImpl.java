f04ad9cbfab5050c6846b189f80dffa3b13d6a95
Rodrigo Gernohovski <gernohovskirodrigo@gmail.com>
2025-09-16 19:20:14 -0300

    feat: Adiciona fluxo de cadastro de localização

--- src/main/java/br/com/fatec/mogi/inventory_service/coreService/service/impl/LocalizacaoServiceImpl.java
+++ src/main/java/br/com/fatec/mogi/inventory_service/coreService/service/impl/LocalizacaoServiceImpl.java
@@ -1,7 +1,9 @@
 package br.com.fatec.mogi.inventory_service.coreService.service.impl;
 
+import br.com.fatec.mogi.inventory_service.coreService.domain.model.Localizacao;
 import br.com.fatec.mogi.inventory_service.coreService.repository.LocalizacaoRepository;
 import br.com.fatec.mogi.inventory_service.coreService.service.LocalizacaoService;
+import br.com.fatec.mogi.inventory_service.coreService.web.request.CadastrarLocalizacaoRequestDTO;
 import br.com.fatec.mogi.inventory_service.coreService.web.response.BuscarLocalizacaoResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,12 @@ public class LocalizacaoServiceImpl implements LocalizacaoService {
 	@Override
 	public BuscarLocalizacaoResponseDTO buscar() {
 		return new BuscarLocalizacaoResponseDTO(localizacaoRepository.findAll());
+	}
+
+	@Override
+	public void cadastrar(CadastrarLocalizacaoRequestDTO dto) {
+		Localizacao localizacao = Localizacao.builder().andar(dto.getAndar()).nomeSala(dto.getNomeSala()).build();
+		localizacaoRepository.save(localizacao);
 	}
 
 }
