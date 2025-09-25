4f4ed661cd2cd2e9cead4936f2f70492dcd86ea8
Rodrigo Gernohovski <gernohovskirodrigo@gmail.com>
2025-09-24 21:13:45 -0300

    fix: Corrige migrations

--- src/main/resources/db/migration/V03__adiciona_funcionalidade_edicao_item.sql
+++ src/main/resources/db/migration/V03__adiciona_funcionalidade_edicao_item.sql
@@ -1,4 +1,4 @@
 INSERT INTO funcionalidade (funcionalidade, endpoint, http_method) VALUES
-    ('ATUALIZAR_ITEM', '/core-service/v1/itens', 'PUT'),
+    ('ATUALIZAR_ITEM', '/core-service/v1/itens', 'PUT');
 
 DELETE FROM funcionalidade WHERE funcionalidade = 'CADASTRO_USUARIO_RELACIONADO';
---
+++ src/main/resources/db/migration/V05__atualiza_permissao_admin.sql
@@ -0,0 +1,11 @@
+INSERT INTO funcao_funcionalidade (funcao_id, funcionalidade_id)
+SELECT
+  (SELECT id FROM funcao WHERE nome = 'ADMIN') as funcao_id,
+  f.id as funcionalidade_id
+FROM funcionalidade f
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM funcao_funcionalidade ff
+  WHERE ff.funcao_id = (SELECT id FROM funcao WHERE nome = 'ADMIN')
+  AND ff.funcionalidade_id = f.id
+);
