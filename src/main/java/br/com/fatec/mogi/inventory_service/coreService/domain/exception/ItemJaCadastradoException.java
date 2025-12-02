29c19092a1a34dd3fa159bc7d09b5a6fb4586daa
Rodrigo Gernohovski <gernohovskirodrigo@gmail.com>
2025-12-02 20:07:40 -0300

    fix: Corrige mensagem de item já cadastrado

--- src/main/java/br/com/fatec/mogi/inventory_service/coreService/domain/exception/ItemJaCadastradoException.java
+++ src/main/java/br/com/fatec/mogi/inventory_service/coreService/domain/exception/ItemJaCadastradoException.java
@@ -3,7 +3,7 @@ package br.com.fatec.mogi.inventory_service.coreService.domain.exception;
 public class ItemJaCadastradoException extends RuntimeException {
 
 	public ItemJaCadastradoException() {
-		super("O item para esse codigo ja foi cadastrado.");
+		super("Já existe um código cadastrado para esse número de patrimônio.");
 	}
 
 }
